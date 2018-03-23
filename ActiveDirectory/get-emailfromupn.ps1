98121210d41d6d5cae9251ccc5e64f32465f0598
James Noonan <james.c.noonan@gmail.com>
2018-03-23 15:41:46 -0400

    old script for grabbing email from a provided upns
    Picks up provided upn from a csv file

---
+++ ActiveDirectory/get-emailfromupn.ps1
@@ -0,0 +1,30 @@
+﻿
+function Get-EmailFromUpn
+{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[string]
+		$csvfilepath
+	)
+	[CMdletBinding[]]
+	
+	$importObjs = Import-Csv -Path $csvfilepath
+	
+	$importObjs | ForEach-Object{
+		
+		$upn = $_.Upn
+		try{ $getuser = $true; $getprops = Get-ADUser -Properties * -Filter { UserPrincipalName -eq $upn } -ErrorAction Stop }
+		catch { $getuser = $false;  $getusererror = $Error.Exception.Message}
+		if ($getuser -eq $true)
+		{
+			[PSCustomObject]@{ user = $getprops.userprincipalname; email = $getprops.emailaddress }
+		}
+		else
+		{
+			[PSCustomObject]@{ Error = $getusererror }
+		}
+		
+	}
+}
+Get-OCCMEmailFromUpn
